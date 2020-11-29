bb08b46a1126f7b61c03ec8899e0da379bac121a
Elaphurus <43885619+Lennox-Elaphurus@users.noreply.github.com>
2020-11-29 12:33:03 +0800

    Update pingGlobal.sh

--- pingGlobal.sh
+++ pingGlobal.sh
@@ -1,33 +1,34 @@
-#!/bin/ash
-echo $(date) "test start"
+#!/bin/sh
+
+/bin/echo $(date) "test start"
 i=1
-while [ $i -le 12 ];
+while [ $i -le 3 ];
 do
-	echo $(date) sleeping $i/30 
-	sleep 10
+	/bin/echo $(date) sleeping $i/3
+	/bin/sleep 10
 	i=$(($i+1))
 done
-#等待30*10秒
+#等待3*10秒
 
 while [ true ];
 do
 	i=1
-	while [ $i -le 18 ];
+	while [ $i -le 6 ];
 	do
-		echo $(date) sleeping $i/18
-		sleep 10
+		/bin/echo $(date) sleeping $i/6
+		/bin/sleep 10
 		i=$(($i+1))
 	done
-    #等待18*10秒
+    #等待6*10秒
 
-	ping -w 60 -c 3 -i 2 baidu.com
-    #发起3次ping，每次ping之间间隔2秒，限时60秒
+	ping -w 30 -c 3 -i 2 baidu.com
+    #发起3次ping，每次ping之间间隔2秒，限时30秒
 	if  [ $? -ne 0 ];
 	then
-		echo $(date) error reboot in 180s 
-		sleep 180
-		reboot
+		/bin/echo $(date) error reboot in 60s 
+		/bin/sleepsleep 60
+		/sbin/reboot
 	else
-		echo $(date) baidu.com connected
+		/bin/echo $(date) baidu.com connected
 	fi
 done
