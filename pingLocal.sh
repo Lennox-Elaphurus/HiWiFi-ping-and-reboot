a301531cbc0d0a5bfd0149058b49de32aa659b86
Elaphurus <43885619+Lennox-Elaphurus@users.noreply.github.com>
2020-12-25 15:20:21 +0800

    Update pingLocal.sh

--- pingLocal.sh
+++ pingLocal.sh
@@ -2,8 +2,8 @@
 
 /bin/echo $(date) "test start"
 
-/usr/bin/ping -w 30 -c 3 -i 2 baidu.com
-#发起3次ping，每次ping之间间隔2秒，限时30秒
+/usr/bin/ping -w 10 -c 3 -i 2 google.cn
+#发起3次ping，每次ping之间间隔2秒，限时10秒
 
 if  [ $? -ne 0 ];
 then
@@ -12,6 +12,18 @@ then
 	/bin/sleep 2
 	/sbin/ifconfig eth2.2 up
 	# 重启网线连接
+
+	/bin/sleep 10
+	/usr/bin/ping -w 10 -c 3 -i 2 google.cn
+	# 再次发起3次ping，每次ping之间间隔2秒，限时10秒
+	if  [ $? -ne 0 ];
+	then
+		/bin/echo $(date) error reboot in 30s 
+		/bin/sleep 30
+		/sbin/reboot
+	else
+		/bin/echo $(date) google.cn connected
+	fi
 else
-	/bin/echo $(date) baidu.com connected
+	/bin/echo $(date) google.cn connected
 fi
